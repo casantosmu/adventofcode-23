9e58c524cfde3703f4316eb584a2858376505b06
Carlos Santos <casantosmu@gmail.com>
2023-12-04 01:09:54 +0100

    Add day-03-part-01-js

---
+++ day-03-part-01-js/index.js
@@ -0,0 +1,72 @@
+/**
+ * https://adventofcode.com/2023/day/3
+ */
+
+const isDigit = (char) => !Number.isNaN(Number(char));
+
+const isSymbol = (value) => value !== "." && !isDigit(value);
+
+const isVertical = (a, b) =>
+  a.x <= b.x &&
+  a.x + a.width >= b.x + b.width &&
+  (a.y === b.y - 1 || a.y === b.y + 1);
+
+const isHorizontal = (a, b) =>
+  (a.x === b.x + 1 || a.x + a.width + 1 === b.x + b.width) && a.y === b.y;
+
+const isDiagonal = (a, b) =>
+  (a.x === b.x + 1 || a.x + a.width + 1 === b.x + b.width) &&
+  (a.y === b.y - 1 || a.y === b.y + 1);
+
+const isAdjacent = (a, b) => {
+  return isVertical(a, b) || isHorizontal(a, b) || isDiagonal(a, b);
+};
+
+export const main = (input) => {
+  const numbers = [];
+  const symbols = [];
+
+  input.split("\n").forEach((line, lineIndex) => {
+    let currentNumber = null;
+
+    line.split("").forEach((char, charIndex) => {
+      if (isDigit(char)) {
+        if (currentNumber === null) {
+          currentNumber = {
+            value: char,
+            x: charIndex,
+            y: lineIndex,
+            width: 1,
+          };
+        } else {
+          currentNumber.value += char;
+          currentNumber.width++;
+        }
+
+        const nextCar = line[charIndex + 1];
+        if (!isDigit(nextCar)) {
+          numbers.push(currentNumber);
+          currentNumber = null;
+        }
+      } else if (isSymbol(char)) {
+        symbols.push({
+          x: charIndex,
+          y: lineIndex,
+          width: 1,
+        });
+      }
+    });
+  });
+
+  let sum = 0;
+  for (const number of numbers) {
+    for (const symbol of symbols) {
+      if (isAdjacent(number, symbol)) {
+        sum += Number(number.value);
+        break;
+      }
+    }
+  }
+
+  return sum;
+};
---
+++ day-03-part-01-js/index.test.js
@@ -0,0 +1,18 @@
+import test from "node:test";
+import assert from "node:assert";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import fs from "node:fs";
+import { main } from "./index.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+test("passing test", () => {
+  const inputPath = path.join(__dirname, "input");
+  const input = fs.readFileSync(inputPath, "utf8");
+  const expects = 525181;
+  const result = main(input);
+
+  assert.strictEqual(result, expects);
+});
